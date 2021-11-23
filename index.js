34f4fa44caa433b1559c65fcc0c8dc14efab0a9b
XavierBonnel <x.bonnel@pm.me>
2021-11-23 21:23:38 +0100

    debug rateLimit 3

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const limiter = rateLimit({
   max: 100, // limit each IP to 100 requests per windowMs
   message: "Sorry, it's too much for us ^^",
 });
-app.use(limiter);
+app.use(limiter());
 
 app.set("trust proxy", 1);
 
