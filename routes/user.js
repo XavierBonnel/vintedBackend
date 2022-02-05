b03673d374062199a956d812063897bb771588a8
XavierBonnel <x.bonnel@pm.me>
2022-02-05 23:01:14 +0100

    signup route problem solving in progress

--- routes/user.js
+++ routes/user.js
@@ -10,8 +10,6 @@ const User = require("../models/User");
 
 const cloudinary = require("cloudinary").v2;
 
-
-
 router.post("/user/signup", async (req, res) => {
   try {
     const avatar = req.files.avatar?.path;
@@ -24,7 +22,7 @@ router.post("/user/signup", async (req, res) => {
       folder: "/vinted/users/",
     });
 
-    if (username) {
+    if (username && avatar) {
       const user = await User.findOne({ email: email });
       if (!user) {
         const salt = uid2(64);
@@ -37,6 +35,32 @@ router.post("/user/signup", async (req, res) => {
             username: username,
             phone: phone,
             avatar: imageCloud.secure_url,
+          },
+          token: token,
+          salt: salt,
+          hash: hash,
+        });
+        await newUser.save();
+        res.status(200).json({
+          _id: newUser._id,
+          token: newUser.token,
+          account: newUser.account,
+        });
+      } else {
+        res.status(409).json({ message: "This email already has an account" });
+      }
+    } else if (username && !avatar) {
+      const user = await User.findOne({ email: email });
+      if (!user) {
+        const salt = uid2(64);
+        const hash = SHA256(password + salt).toString(encbase64);
+        const token = uid2(64);
+
+        const newUser = new User({
+          email: email,
+          account: {
+            username: username,
+            phone: phone,
           },
           token: token,
           salt: salt,
@@ -84,6 +108,5 @@ router.post("/user/login", async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
-
 
 module.exports = router;
