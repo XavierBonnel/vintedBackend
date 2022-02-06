67d527ccfdcfc9b1550fe9e384e72d31ed0f090f
XavierBonnel <x.bonnel@pm.me>
2022-02-06 14:07:27 +0100

    signup route problem solved

--- routes/user.js
+++ routes/user.js
@@ -12,44 +12,12 @@ const cloudinary = require("cloudinary").v2;
 
 router.post("/user/signup", async (req, res) => {
   try {
-    const avatar = req.files?.avatar?.path;
     const email = req.fields.email;
     const username = req.fields.username;
     const phone = req.fields.phone;
     const password = req.fields.password;
 
-    const imageCloud = await cloudinary.uploader.upload(avatar, {
-      folder: "/vinted/users/",
-    });
-
-    if (username && avatar) {
-      const user = await User.findOne({ email: email });
-      if (!user) {
-        const salt = uid2(64);
-        const hash = SHA256(password + salt).toString(encbase64);
-        const token = uid2(64);
-
-        const newUser = new User({
-          email: email,
-          account: {
-            username: username,
-            phone: phone,
-            avatar: imageCloud.secure_url,
-          },
-          token: token,
-          salt: salt,
-          hash: hash,
-        });
-        await newUser.save();
-        res.status(200).json({
-          _id: newUser._id,
-          token: newUser.token,
-          account: newUser.account,
-        });
-      } else {
-        res.status(409).json({ message: "This email already has an account" });
-      }
-    } else if (username && !avatar) {
+    if (username) {
       const user = await User.findOne({ email: email });
       if (!user) {
         const salt = uid2(64);
@@ -66,6 +34,17 @@ router.post("/user/signup", async (req, res) => {
           salt: salt,
           hash: hash,
         });
+
+        if (req.files.avatar) {
+          const imageCloud = await cloudinary.uploader.upload(
+            req.files.avatar.path,
+            {
+              folder: "/vinted/users/",
+            }
+          );
+          newUser.account.avatar = imageCloud.secure_url;
+        }
+
         await newUser.save();
         res.status(200).json({
           _id: newUser._id,
@@ -75,8 +54,6 @@ router.post("/user/signup", async (req, res) => {
       } else {
         res.status(409).json({ message: "This email already has an account" });
       }
-    } else {
-      res.status(400).json({ message: "Missing username parameter" });
     }
   } catch (error) {
     res.status(400).json({ message: error.message });
